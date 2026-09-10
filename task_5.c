9be7a65f2227db125a6603e252ba0ef34cd7aa7c
bohdan123 <zamih26@gmail.com>
2026-09-10 14:58:59 +0300

    Add matrix task

---
+++ task_5.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main(){
+
+    int max, min;
+    printf("Enter the minimum value: ");
+    scanf("%d", &min);
+    printf("Enter the maximum value: ");
+    scanf(" %d", &max);
+    for(int i = min; i <= max; i++){
+        printf("%d ", i);
+    }
+
+
+
+
+
+
+
+
+    return 0;
+}
